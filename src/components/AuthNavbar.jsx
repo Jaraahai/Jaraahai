304d1b4ffacdcf230886d7964e40f48d97dc70ee
Jaraahai <pcatstwo@Gmail.com>
2023-10-22 19:42:30 +0800

    Fixed errors in AuthNavbar.jsx

--- src/components/AuthNavbar.jsx
+++ src/components/AuthNavbar.jsx
@@ -1,62 +1,81 @@
 import React from "react";
+import "../App.css";
 import { Fragment } from "react";
-// import { useLocation } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebaseConfig";
+import { useNavigate } from "react-router-dom";
 // import useAuthState from "../hooks/useAuthState";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import profilePic from "../img/profile.png";
 
-const navigation = [{ name: "Dashboard", href: "/dashboard", current: true }];
+const navigation = [{ name: "Dashboard", href: "#", current: true }];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 function AuthNavbar() {
-  // const location = useLocation();
+  const navigate = useNavigate();
   // const { user } = useAuthState();
 
   // if (location.pathname === "/auth" || user) {
   //   return null;
   // }
 
+  const signUserOut = async () => {
+    try {
+      await signOut(auth);
+      localStorage.clear();
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="tw-bg-[#1f1f1f]">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
+          <div className="tw-mx-auto tw-max-w-7xl tw-px-2 tw-sm:px-6 tw-lg:px-8">
+            <div className="tw-p-4 tw-flex tw-h-16 tw-items-center tw-justify-between">
+              {/* <div className="tw-absolute tw-inset-y-0 tw-left-0 tw-flex tw-items-center tw-sm:hidden"> */}
+              {/* Mobile menu button*/}
+              {/* <Disclosure.Button className="tw-relative tw-inline-flex tw-items-center tw-justify-center tw-rounded-md tw-p-2 tw-text-gray-400 tw-hover:bg-gray-700 tw-hover:text-white tw-focus:outline-none tw-focus:ring-2 tw-focus:ring-inset tw-focus:ring-white">
+                  <span className="tw-absolute tw--inset-0.5" />
+                  <span className="tw-sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon
+                      className="tw-block tw-h-6 tw-w-6"
+                      aria-hidden="true"
+                    />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon
+                      className="tw-block tw-h-6 tw-w-6"
+                      aria-hidden="true"
+                    />
                   )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+                </Disclosure.Button> */}
+              {/* </div> */}
+              <div className="tw-flex tw-flex-1 tw-items-center tw-justify-center tw-sm:items-stretch tw-sm:justify-start">
+                <div className="tw-space-x-2 tw-flex tw-flex-shrink-0 tw-items-center">
                   <img
-                    className="h-8 w-auto"
+                    className="tw-h-8 tw-w-auto"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
+                <div className="tw-hidden tw-sm:ml-6 tw-sm:block">
+                  <div className="tw-flex tw-space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
+                            ? "tw-bg-gray-900 tw-text-white"
+                            : "tw-text-gray-300 tw-hover:bg-gray-700 tw-hover:text-white",
+                          "tw-rounded-md tw-px-3 tw-py-2 tw-text-sm tw-font-medium"
                         )}
                         aria-current={item.current ? "page" : undefined}
                       >
@@ -66,7 +85,7 @@ function AuthNavbar() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="tw-space-x-2 tw-inset-y-0 tw-right-0 tw-flex tw-items-center tw-pr-2 tw-sm:static tw-sm:inset-auto tw-sm:ml-6 tw-sm:pr-0">
                 {/* <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -77,13 +96,13 @@ function AuthNavbar() {
                 </button> */}
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
+                <Menu as="div" className="tw-relative tw-ml-3">
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
+                    <Menu.Button className="tw-relative tw-flex tw-rounded-full tw-bg-gray-800 tw-text-sm tw-focus:outline-none tw-focus:ring-2 tw-focus:ring-white tw-focus:ring-offset-2 tw-focus:ring-offset-gray-800">
+                      <span className="tw-absolute tw--inset-1.5" />
+                      <span className="tw-sr-only">Open user menu</span>
                       <img
-                        className="h-8 w-8 rounded-full"
+                        className="tw-h-8 tw-w-8 tw-rounded-full"
                         src={profilePic}
                         alt=""
                       />
@@ -98,14 +117,14 @@ function AuthNavbar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="tw-absolute tw-right-0 tw-z-10 tw-mt-2 tw-w-48 tw-origin-top-right tw-rounded-md tw-bg-white tw-py-1 tw-shadow-lg tw-ring-1 tw-ring-black tw-ring-opacity-5 tw-focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
                             href="/profile"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "tw-bg-gray-100" : "",
+                              "tw-block tw-px-4 tw-py-2 tw-text-sm tw-text-gray-700"
                             )}
                           >
                             Your Profile
@@ -130,9 +149,10 @@ function AuthNavbar() {
                           <a
                             href="/"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "tw-bg-gray-100" : "",
+                              "tw-block tw-px-4 tw-py-2 tw-text-sm tw-text-gray-700"
                             )}
+                            onClick={signUserOut}
                           >
                             Sign out
                           </a>
@@ -145,8 +165,8 @@ function AuthNavbar() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+          <Disclosure.Panel className="tw-sm:hidden">
+            <div className="tw-space-y-1 tw-px-2 tw-pb-3 tw-pt-2">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
@@ -154,9 +174,9 @@ function AuthNavbar() {
                   href={item.href}
                   className={classNames(
                     item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
+                      ? "tw-bg-gray-900 tw-text-white"
+                      : "tw-text-gray-300 tw-hover:bg-gray-700 tw-hover:text-white",
+                    "tw-block tw-rounded-md tw-px-3 tw-py-2 tw-text-base tw-font-medium"
                   )}
                   aria-current={item.current ? "page" : undefined}
                 >
--- src/hooks/useAddProfile.js
+++ src/hooks/useAddProfile.js
@@ -1,19 +1,19 @@
-// import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-// import { db } from "../config/firebaseConfig";
-// import { useGetUserInfo } from "./useGetUserInfo";
-// export const useAddProfileData = () => {
-//   const profileCollectionRef = collection(db, "profile");
-//   const { userID } = useGetUserInfo();
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
+import { useGetUserInfo } from "./useGetUserInfo";
+export const useAddProfileData = () => {
+  const profileCollectionRef = collection(db, "profile");
+  const { userID } = useGetUserInfo();
 
-//   const profileData = async ({ name, age, rank }) => {
-//     await addDoc(profileCollectionRef, {
-//       userID,
-//       name,
-//       age,
-//       rank,
-//       createdAt: serverTimestamp(),
-//     });
-//   };
+  const profileData = async ({ name, age, rank }) => {
+    await addDoc(profileCollectionRef, {
+      userID,
+      name,
+      age,
+      rank,
+      createdAt: serverTimestamp(),
+    });
+  };
 
-//   return { profileData };
-// };
+  return { profileData };
+};
